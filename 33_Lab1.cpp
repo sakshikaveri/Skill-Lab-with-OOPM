8edd1faa934e9f5f11b69699af8cbe597cc04818
sakshikaveri <90851879+sakshikaveri@users.noreply.github.com>
2021-09-17 18:07:30 +0530

    Add files via upload

---
+++ 33_Lab1.cpp
@@ -0,0 +1,61 @@
+/          // TO ADD TWO NUMBERS
+ #include<iostream>
+using namespace std;
+int main()
+
+{
+	int n1,n2,result;
+	n1=10,n2=15;
+	result=n1+n2;
+	cout<<n1<<" + "<<n2<<" = "<<result<<endl;
+          	//TO PRINT TWO NUMBERS
+  	int num1,num2;
+	cout<<"enter two numbers ";
+	cin>>num1>>num2;
+	cout<<"the entered numbers are: "<<num1<<" and "<<num2;
+	return 0;
+}
+                // TO SWAP TWO NUMBERS
+/*	#include<iostream>
+	 using namespace std;
+	 int main()
+	 {
+	 
+    	int n1,n2,temp;
+		cout<<"enter the numbers ";
+		cin>>n1>>n2;
+		temp=n1;
+		n1=n2;
+		n2=temp;
+		cout<<"the swapped numbers are: "<<n1<< " and "<<n2<<endl;
+		
+	      // TO CHECK WHETHER THE GIVEN NUMBER IS EVEN OR ODD
+	      	
+        int num1;
+ 	cout<<"enter the given number";
+	cin>>num1;
+	if(num1%2==0)
+	{
+		cout<<"the number "<<num1<<" is even";
+	}
+	else
+	{
+		cout<<"the number "<<num1<<" is odd";
+	}
+	return 0;
+} */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
