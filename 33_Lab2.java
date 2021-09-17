deb439ce3e85487ac2e34667b33be71d23490da8
sakshikaveri <90851879+sakshikaveri@users.noreply.github.com>
2021-09-17 18:02:22 +0530

    Add files via upload

---
+++ 33_Lab2.java
@@ -0,0 +1,61 @@
+                     //TO ADD TWO NUMBERS
+import java.util.Scanner;
+public class Lab1 {
+	public static void main (String[] args) {
+ 
+		int n1,n2,result;
+		n1=10;
+		n2=15;
+		result=n1+n2; 
+		System.out.println(n1+" + "+n2+" = "+result); 
+	
+
+                     //TO PRINT THE USER ENTERED NUMBER
+
+		/*int num1,num2;
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter first number ");
+		num1=sc.nextInt();
+		System.out.println("enter second number ");
+		num2=sc.nextInt();
+		System.out.println("first number is="+num1);
+		System.out.println("second number is="+num2);*/
+	
+
+                   // TWO SWAP TWO NUMBERS
+
+		/*int n1,n2,temp;
+		Scanner sc= new Scanner(System.in);
+		System.out.println("enter first number");
+		n1=sc.nextInt();
+		System.out.println("enter second number");
+		n2=sc.nextInt();
+		temp=n1;
+		n1=n2;
+		n2=temp;
+		System.out.println("first number is="+n1);
+		System.out.println("second number is="+n2); */
+	
+
+            // TWO CHECK WHETHER THE NUMBER IS EVEN OR ODD
+
+		
+		/*int a;
+		Scanner sc= new Scanner(System.in);
+		System.out.println("enter the number");
+		a=sc.nextInt();
+		if(a%2==0)
+		{
+			System.out.println("the number "+a+" is even");
+			}
+		else
+		{
+			System.out.println("the number "+a+" is odd");
+		}*/
+		
+		
+		
+		
+		
+}
+	}
